be7356bffcccfec00a16c2eb63e2fa4d31c822e6
Ralf Sippl <ralf.sippl@gmail.com>
2019-08-29 11:06:45 +0200

    Change layers dir name

--- clone-layers.sh
+++ clone-layers.sh
@@ -3,17 +3,21 @@
 set -e
 
 workspace_dir="workspace"
+layers_dir="layers"
 
 # TODO set to the Yocto release you want to use, e.g. thud or warrior
 yocto_release="thud"
 
-mkdir -p "${workspace_dir}/sources" && cd "${workspace_dir}"
+mkdir -p "${workspace_dir}/${layers_dir}" && cd "${workspace_dir}"
 
 # clone default layers: poky, OE etc.
-git clone -b ${yocto_release} git://git.yoctoproject.org/poky.git sources/poky
-git clone -b ${yocto_release} https://github.com/openembedded/meta-openembedded.git sources/meta-openembedded
+git clone -b ${yocto_release} git://git.yoctoproject.org/poky.git ${layers_dir}/poky
+git clone -b ${yocto_release} https://github.com/openembedded/meta-openembedded.git ${layers_dir}/meta-openembedded
 
 # TODO clone additional layers here, e.g. for Raspberry Pi, use this
-# git clone -b ${yocto_release} https://github.com/agherzan/meta-raspberrypi.git sources/meta-raspberrypi
+# git clone -b ${yocto_release} https://github.com/agherzan/meta-raspberrypi.git ${layers_dir}/meta-raspberrypi
 
-echo "Done, type \"cd ${workspace_dir} && . ./sources/poky/oe-init-build-env\" to create the build environment"
+# Or use repo init instead, e.g for ST:
+# repo init -u https://github.com/STMicroelectronics/oe-manifest.git -b refs/tags/openstlinux-4.19-thud-mp1-19-02-20
+
+echo "Done, type \"cd ${workspace_dir} && . ./${layers_dir}/poky/oe-init-build-env\" to create the build environment"
